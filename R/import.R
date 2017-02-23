6e5ab1c3787d7239d204e2ab16408a38113d63c3
frlender <qiaonan@MacBook-Pro-2.local>
2017-02-23 10:35:08 -0800

    add deparse

--- R/import.R
+++ R/import.R
@@ -2,11 +2,16 @@
 #'
 #' This function allows you to import a R file as a module like in python
 #' so that its content won't contaminate your working directory.
-#' @path the path of the R file.
-#' import()
+#' @param path the path of the R file.
+#' @keywords import
+#' @export
+#' @examples
+#' import('xxx.R')
+#' import(xxx.R)
 
 import <- function(path){
   such <- new.env()
+  path <- as.character(substitute(path))
   source(path,such)
   such
 }
